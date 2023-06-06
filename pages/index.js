ed50faa33aca8fdaf876b1bec31fb5894830faf4
oncet <camilorivera86@gmail.com>
2023-06-05 23:13:32 -0300

    wip

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,45 @@
 export default function Home() {
-  return <main>Hello world!</main>;
+  return (
+    <main>
+      <h1>Camilo Rivera</h1>
+      <p>
+        Experienced developer with a focus on delivering high-quality code and
+        great user experience. Proficient in a range of technologies, including
+        TypeScript, React, Node.js, and SQL. Experience in project management
+        and effective communication.
+      </p>
+      <h2>Work experience</h2>
+      <dl>
+        <dt>Full-stack Engineer (jun 2021 — dec 2022) @ Sr. Pago/Konfio</dt>
+        <dd>
+          <ul>
+            <li>
+              Collaborated with the development team to successfully migrate a
+              legacy user dashboard front-end from PHP to React, resulting in a
+              reduction in page load times. Contributed to the development of
+              key features, such as responsive design, and data visualization.
+            </li>
+            <li>
+              Participated in the development of a new Authentication API
+              microservice using Nest.js. Worked with a team of developers and
+              contributed to the design and implementation of key features, such
+              as user authentication, authorization, and password recovery.
+            </li>
+            <li>
+              Collaborated with the Component Library team to develop and
+              maintain reusable UI elements, design tokens, and accessibility
+              guidelines for React components using Material UI, TailwindCSS,
+              and Storybook, contributing to a more efficient and cohesive
+              design system for the company.
+            </li>
+            <li>
+              Worked collaboratively with a team of developers and designers to
+              develop new features and fix bugs on the front-end of the main
+              product using React.
+            </li>
+          </ul>
+        </dd>
+      </dl>
+    </main>
+  );
 }
