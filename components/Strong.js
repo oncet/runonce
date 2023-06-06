bf66f874500f1dafd538d211293e9bd26ced26c0
oncet <camilorivera86@gmail.com>
2023-06-06 19:50:57 -0300

    add more styling

---
+++ components/Strong.js
@@ -0,0 +1,3 @@
+export default function Strong({ children }) {
+  return <strong className="font-medium text-sky-400">{children}</strong>;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Strong from "@/components/Strong";
+
 export default function Home() {
   return (
     <main className="flex flex-col gap-4">
@@ -5,10 +7,8 @@ export default function Home() {
       <p className="text-lg text-slate-400">
         Experienced developer with a focus on delivering high-quality code and
         great user experience. Proficient in a range of technologies, including{" "}
-        <strong className="font-medium text-sky-400">TypeScript</strong>,{" "}
-        <strong className="font-medium text-sky-400">React</strong>,{" "}
-        <strong className="font-medium text-sky-400">Node.js</strong>, and{" "}
-        <strong className="font-medium text-sky-400">SQL</strong>. Experience in
+        <Strong>TypeScript</Strong>, <Strong>React</Strong>,{" "}
+        <Strong>Node.js</Strong>, and <Strong>SQL</Strong>. Experience in
         project management and effective communication.
       </p>
       <h2 className="text-3xl font-bold">Work experience</h2>
@@ -43,6 +43,30 @@ export default function Home() {
             Worked collaboratively with a team of developers and designers to
             develop new features and fix bugs on the front-end of the main
             product using <strong>React</strong>.
+          </li>
+        </ul>
+      </div>
+      <div className="flex flex-col gap-4">
+        <div>
+          <h3 className="text-xl font-semibold">Backend engineer</h3>
+          <p className="text-lg text-slate-400">nov 2020 — may 2021</p>
+          <p className="text-lg text-slate-400">Yappa World Inc.</p>
+        </div>
+        <ul className="flex list-disc flex-col gap-4 pl-4">
+          <li>
+            Worked on the development and maintenance of microservices and
+            gateway{" "}
+            <strong>REST APIs using TypeScript, Nest.js, and MySQL</strong>.
+          </li>
+          <li>
+            Implemented NATS as a messaging service between microservices,
+            enhancing the reliability and performance of inter-service
+            communication within the system.
+          </li>
+          <li>
+            Collaborated on front-end tasks using Vue.js, contributing to the
+            development and maintenance of high-quality user interfaces for the
+            company&apos;s product.
           </li>
         </ul>
       </div>
