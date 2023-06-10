1fdda24da62e82e100373edc716c21d0c92ce482
oncet <camilorivera86@gmail.com>
2023-06-10 01:40:29 -0300

    add online profiles section

--- components/Ul.js
+++ components/Ul.js
@@ -1,3 +1,3 @@
-export default function Ul({ children }) {
-  return <ul className="flex list-disc flex-col gap-4 pl-4">{children}</ul>;
+export default function Ul({ condensed = false, children }) {
+  return <ul className={"flex list-disc flex-col pl-4 " + (condensed ? 'gap-1' : 'gap-4')}>{children}</ul>;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Strong from "@/components/Strong";
+import Ul from "@/components/Ul";
 import Elementum from "@/components/experience/Elementum";
 import Konfio from "@/components/experience/Konfio";
 import Yappa from "@/components/experience/Yappa";
@@ -27,6 +28,12 @@ export default function Home() {
           </div>
         ))}
       </div>
+      <h2 className="text-3xl font-bold">Online profiles</h2>
+      <Ul condensed>
+        <li><a href="https://github.com/oncet/" className="text-slate-400">https://github.com/oncet/</a></li>
+        <li><a href="https://www.linkedin.com/in/camilorivera/" className="text-slate-400">https://www.linkedin.com/in/camilorivera/</a></li>
+        <li><a href="https://stackoverflow.com/users/1140065/camilo/" className="text-slate-400">https://stackoverflow.com/users/1140065/camilo/</a></li>
+      </Ul>
     </main>
   );
 }
