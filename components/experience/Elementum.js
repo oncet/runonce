623348db6334479dfbf351fee1e21613094476cb
oncet <camilorivera86@gmail.com>
2023-06-10 01:14:20 -0300

    update elementum experience

--- components/experience/Elementum.js
+++ components/experience/Elementum.js
@@ -7,7 +7,7 @@ export default function Elementum() {
   return (
     <>
       <div>
-        <H3>Front-end engineer</H3>
+        <H3>Frontend engineer</H3>
         <Subtitle>may 2019 — nov 2020</Subtitle>
         <Subtitle>Elementum</Subtitle>
       </div>
@@ -16,16 +16,18 @@ export default function Elementum() {
           Worked on the development and maintenance of supply chain management products, contributing to the design and implementation of key features.
         </li>
         <li>
-          Collaborated with the Component Library team by adding and maintaining React components using styled-components and Storybook, contributing to a more cohesive design system for the company&apos;s products.
+          Collaborated on developing and maintaining reusable UI elements,
+          design tokens, and accessibility guidelines for a component library
+          using <Strong>React</Strong>, <Strong>styled-components</Strong> and <Strong>Storybook</Strong>.
         </li>
         <li>
-          Optimized React and Webpack configurations to improve app performance, resulting in a ~75% reduction in npm install time and a ~70% improvement in overall Lighthouse score. Contributed to the development and implementation of key optimizations, such as tree shaking, code splitting, and lazy loading.
+          Fine tuned <Strong>webpack</Strong> configurations and implemented other key optimizations, such as tree shaking, code splitting, and lazy loading. The results included a 75% reduction in <code>npm install</code> time and a 70% improvement in overall Lighthouse score, enhancing both the user experience and the developer experience.
         </li>
         <li>
-          Implemented Module Federation as a potential replacement for an in-house micro frontend solution, exploring and evaluating new technologies to improve the company&apos;s front-end architecture.
+          Implemented Module Federation as a potential replacement for an in-house micro frontend solution, exploring and evaluating new technologies to improve the frontend architecture of the product.
         </li>
         <li>
-          Worked on the development and maintenance of microservices REST APIs using Node.js (Express) and MongoDB.
+          Worked on the development and maintenance of microservices <Strong>REST APIs</Strong> using <Strong>Node.js</Strong>, <Strong>Express</Strong>, and <Strong>MongoDB</Strong>.
         </li>
       </Ul>
     </>
