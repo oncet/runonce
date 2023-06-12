6670a02fb0573648c77b04f7133545271b9a2a7f
oncet <camilorivera86@gmail.com>
2023-06-11 22:26:55 -0300

    toggle card icon

--- pages/cards.js
+++ pages/cards.js
@@ -38,7 +38,11 @@ export default function Home() {
             </div>
             <p className="text-right">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 stroke-slate-400">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                {isOpen ? (
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12h-15" />
+                ) : (
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                )}
               </svg>
             </p>
           </div>
