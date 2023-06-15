365bb1a17dacc1bec8ff8135f5401df95cadf951
oncet <camilorivera86@gmail.com>
2023-06-15 18:15:58 -0300

    update index

--- pages/index.js
+++ pages/index.js
@@ -13,28 +13,14 @@ export default function Home() {
         />
       </Head>
       <h1 className="text-4xl font-extrabold tracking-tight">runonce</h1>
-      <p className="text-lg">
-        Welcome to my homepage. Please take a look at my{" "}
+      <p className="text-lg text-slate-300 [text-wrap:balance]">
+        Welcome! Hopefully there will be more content here in the future,
+        meanwhile you can checkout my{" "}
         <Link href="resume" className="font-bold text-sky-400">
           resume
         </Link>
         .
       </p>
-      <p className="text-lg text-slate-400">
-        Hopefully there will be more content here in the future.
-      </p>
-      {/* <blockquote className="my-20">
-        <p className="py-4 text-right text-3xl font-extrabold tracking-tight">
-          &quot;You should name a variable using the same care with which you
-          name a first-born child.&quot;
-        </p>
-        <figcaption className="mt-4 text-right">
-          <div className="font-bold">Robert C. Martin</div>
-          <cite className="text-sm not-italic text-slate-400">
-            Clean Code: A Handbook of Agile Software Craftsmanship
-          </cite>
-        </figcaption>
-      </blockquote> */}
     </main>
   );
 }
