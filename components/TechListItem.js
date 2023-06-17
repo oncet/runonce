4556c04d82df43b2aad7b737bc0de6439067eb93
oncet <camilorivera86@gmail.com>
2023-06-17 14:08:45 -0300

    reduce tech list items padding

--- components/TechListItem.js
+++ components/TechListItem.js
@@ -1,6 +1,6 @@
 export default function TechListItem({ children }) {
   return (
-    <li className="rounded-lg border border-slate-700 px-3 py-2 font-mono text-sky-400">
+    <li className="rounded-lg border border-slate-700 px-2 py-1 font-mono text-sky-400">
       {children}
     </li>
   );
