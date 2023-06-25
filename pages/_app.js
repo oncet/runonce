b315b5e68a1ee356c4b9cc16bc6f736d0695d68e
oncet <camilorivera86@gmail.com>
2023-06-25 12:44:32 -0300

    use next themes for theme toggling

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import { Analytics } from "@vercel/analytics/react";
+import { ThemeProvider } from "next-themes";
 
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Component {...pageProps} />
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
       <Analytics />
     </>
   );
--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="bg-slate-50 text-slate-900 dark:bg-slate-900 dark:text-slate-200 print:!text-black">
         <Main />
         <NextScript />
       </body>
