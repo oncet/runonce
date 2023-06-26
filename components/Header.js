5bcb06230865816469695cfc0ad21eaa8c70b301
oncet <camilorivera86@gmail.com>
2023-06-25 21:55:25 -0300

    improve header

--- components/Header.js
+++ components/Header.js
@@ -1,94 +1,20 @@
-import { useTheme } from "next-themes";
 import Image from "next/image";
-import { useEffect, useState } from "react";
 
 import Strong from "./Strong";
-import ComputerIcon from "./icons/ComputerIcon";
-import MoonIcon from "./icons/MoonIcon";
-import SunIcon from "./icons/SunIcon";
+import ToggleDarkModeButton from "./ToggleDarkModeButton";
+import Link from "next/link";
 
-export default function Header({ children }) {
-  const { theme, setTheme } = useTheme();
-  const [mounted, setMounted] = useState(false);
-  const [isThemeMenuOpen, setIsThemeMenuOpen] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  const onClickHandler = () => {
-    setIsThemeMenuOpen(!isThemeMenuOpen);
-  };
-
+export default function Header() {
   return (
     <header className="">
-      <div className="border-b border-slate-200 px-2 dark:border-slate-800">
-        <div className="mx-auto max-w-screen-md text-right">
-          {mounted ? (
-            <div className="relative inline-block rounded-full print:hidden">
-              <button
-                className="rounded-full p-3 [-webkit-tap-highlight-color:transparent]"
-                onClick={onClickHandler}
-              >
-                {theme === "light" ? <SunIcon /> : ""}
-                {theme === "dark" ? <MoonIcon /> : ""}
-                {theme === "system" ? <ComputerIcon /> : ""}
-              </button>
-              {isThemeMenuOpen ? (
-                <ul className="absolute mt-2 flex flex-col gap-1 overflow-hidden rounded-full bg-slate-200 text-center  dark:bg-slate-800">
-                  {theme !== "light" ? (
-                    <li>
-                      <button
-                        className="rounded-full p-3 [-webkit-tap-highlight-color:transparent]"
-                        onClick={() => {
-                          setTheme("light");
-                          setIsThemeMenuOpen(false);
-                        }}
-                      >
-                        <SunIcon />
-                      </button>
-                    </li>
-                  ) : (
-                    ""
-                  )}
-                  {theme !== "dark" ? (
-                    <li>
-                      <button
-                        className="rounded-full p-3 [-webkit-tap-highlight-color:transparent]"
-                        onClick={() => {
-                          setTheme("dark");
-                          setIsThemeMenuOpen(false);
-                        }}
-                      >
-                        <MoonIcon />
-                      </button>
-                    </li>
-                  ) : (
-                    ""
-                  )}
-                  {theme !== "system" ? (
-                    <li>
-                      <button
-                        className="rounded-full p-3 [-webkit-tap-highlight-color:transparent]"
-                        onClick={() => {
-                          setTheme("system");
-                          setIsThemeMenuOpen(false);
-                        }}
-                      >
-                        <ComputerIcon />
-                      </button>
-                    </li>
-                  ) : (
-                    ""
-                  )}
-                </ul>
-              ) : (
-                ""
-              )}
-            </div>
-          ) : (
-            ""
-          )}
+      <div className="border-b border-slate-200 dark:border-slate-800 print:hidden">
+        <div className="mx-auto flex max-w-screen-md items-center justify-between px-4 text-right">
+          <div>
+            <Link href="/" className="text-slate-400">
+              runonce.io
+            </Link>
+          </div>
+          <ToggleDarkModeButton />
         </div>
       </div>
       <div className="mx-auto max-w-screen-md print:flex print:flex-row print:justify-between">
