1abfca6a826af1aec77eeb096795f1127318de7a
oncet <camilorivera86@gmail.com>
2023-06-26 02:24:10 -0300

    cleanup

--- components/ToggleDarkModeButton.js
+++ components/ToggleDarkModeButton.js
@@ -24,42 +24,39 @@ export default function ToggleDarkModeButton() {
     setIsThemeMenuOpen(!isThemeMenuOpen);
   };
 
-  return mounted ? (
-    <div className="relative inline-block rounded-full print:hidden">
-      <button
-        className={
-          "rounded-full p-3 [-webkit-tap-highlight-color:transparent] " +
-          (["light", "dark"].includes(theme) ? "text-sky-600" : "")
-        }
-        onClick={onClickHandler}
-      >
-        {resolvedTheme === "light" ? <SunIcon /> : ""}
-        {resolvedTheme === "dark" ? <MoonIcon /> : ""}
-      </button>
-      {isThemeMenuOpen ? (
-        <ul className="absolute mt-2 flex flex-col gap-1 overflow-hidden rounded-full bg-slate-200 text-center  dark:bg-slate-800">
-          {Object.keys(themeIcons).map((themeName) => (
-            <li key={themeName}>
-              <button
-                className={
-                  "rounded-full p-3 [-webkit-tap-highlight-color:transparent] " +
-                  (themeName === theme ? "text-sky-600" : "")
-                }
-                onClick={() => {
-                  setTheme(themeName);
-                  setIsThemeMenuOpen(false);
-                }}
-              >
-                {themeIcons[themeName]}
-              </button>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        ""
-      )}
-    </div>
-  ) : (
-    ""
+  return (
+    mounted && (
+      <div className="relative inline-block rounded-full print:hidden">
+        <button
+          className={
+            "rounded-full p-3 [-webkit-tap-highlight-color:transparent] " +
+            (["light", "dark"].includes(theme) ? "text-sky-600" : "")
+          }
+          onClick={onClickHandler}
+        >
+          {resolvedTheme === "light" ? <SunIcon /> : <MoonIcon />}
+        </button>
+        {isThemeMenuOpen && (
+          <ul className="absolute mt-2 flex flex-col gap-1 overflow-hidden rounded-full bg-slate-200 text-center  dark:bg-slate-800">
+            {Object.keys(themeIcons).map((themeName) => (
+              <li key={themeName}>
+                <button
+                  className={
+                    "rounded-full p-3 [-webkit-tap-highlight-color:transparent] " +
+                    (themeName === theme ? "text-sky-600" : "")
+                  }
+                  onClick={() => {
+                    setTheme(themeName);
+                    setIsThemeMenuOpen(false);
+                  }}
+                >
+                  {themeIcons[themeName]}
+                </button>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    )
   );
 }
