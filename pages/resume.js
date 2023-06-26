73910627e2e447854b6afcb9140a468d56dde789
oncet <camilorivera86@gmail.com>
2023-06-25 21:35:57 -0300

    remove div

--- pages/resume.js
+++ pages/resume.js
@@ -27,69 +27,67 @@ export default function Home() {
           key="desc"
         />
       </Head>
-      <div>
-        <Header />
-        <main>
-          <div className="mx-auto flex max-w-screen-md flex-col gap-7 px-4 print:max-w-none print:gap-3 print:px-0">
-            <Konfio />
-            <Yappa />
-            <Elementum />
-            <Educar2018 />
-            <Gm2dev />
-            <div>
-              <div className="relative text-center print:hidden">
-                <hr
-                  className={
-                    "border-1 absolute inset-y-1/2 w-full transition-opacity duration-1000 dark:border-slate-800 " +
-                    (isExperienceExpanded ? "opacity-100" : "opacity-0")
-                  }
-                />
-                <button
-                  onClick={() => setIsExperienceExpanded(true)}
-                  disabled={isExperienceExpanded}
-                  className={
-                    "relative rounded-lg border border-slate-300 bg-slate-200 px-4 py-2 font-semibold text-sky-600 transition-opacity [-webkit-tap-highlight-color:transparent] dark:border-slate-700 dark:bg-slate-800 dark:text-sky-500 " +
-                    (isExperienceExpanded ? "opacity-0" : "opacity-100")
-                  }
-                >
-                  View more
-                </button>
-              </div>
-              <div
+      <Header />
+      <main>
+        <div className="mx-auto flex max-w-screen-md flex-col gap-7 px-4 print:max-w-none print:gap-3 print:px-0">
+          <Konfio />
+          <Yappa />
+          <Elementum />
+          <Educar2018 />
+          <Gm2dev />
+          <div>
+            <div className="relative text-center print:hidden">
+              <hr
                 className={
-                  "grid transition-all duration-1000 ease-in-out print:block print:opacity-100 " +
-                  (isExperienceExpanded
-                    ? "grid-rows-[1fr] opacity-100"
-                    : "grid-rows-[0fr] opacity-0")
+                  "border-1 absolute inset-y-1/2 w-full transition-opacity duration-1000 dark:border-slate-800 " +
+                  (isExperienceExpanded ? "opacity-100" : "opacity-0")
+                }
+              />
+              <button
+                onClick={() => setIsExperienceExpanded(true)}
+                disabled={isExperienceExpanded}
+                className={
+                  "relative rounded-lg border border-slate-300 bg-slate-200 px-4 py-2 font-semibold text-sky-600 transition-opacity [-webkit-tap-highlight-color:transparent] dark:border-slate-700 dark:bg-slate-800 dark:text-sky-500 " +
+                  (isExperienceExpanded ? "opacity-0" : "opacity-100")
                 }
               >
-                <div className="row-[1_/_span_2] overflow-hidden">
-                  <div className="mt-7 flex flex-col gap-7 print:gap-3">
-                    <LeanderGames />
-                    <Educar />
-                    <Persiscal />
-                    <EntornosEducativos />
-                  </div>
+                View more
+              </button>
+            </div>
+            <div
+              className={
+                "grid transition-all duration-1000 ease-in-out print:block print:opacity-100 " +
+                (isExperienceExpanded
+                  ? "grid-rows-[1fr] opacity-100"
+                  : "grid-rows-[0fr] opacity-0")
+              }
+            >
+              <div className="row-[1_/_span_2] overflow-hidden">
+                <div className="mt-7 flex flex-col gap-7 print:gap-3">
+                  <LeanderGames />
+                  <Educar />
+                  <Persiscal />
+                  <EntornosEducativos />
                 </div>
               </div>
             </div>
           </div>
-          <div className="mt-8 bg-slate-200 py-8 text-center dark:bg-slate-800 sm:text-left">
-            <p className="mx-auto max-w-screen-md px-4 text-slate-800 dark:text-slate-400">
-              Contact me at{" "}
-              <a
-                href="mailto:camilorivera86@gmail.com"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="whitespace-nowrap text-lg font-semibold text-sky-600 dark:text-sky-500"
-              >
-                <MailIcon /> camilorivera86@gmail.com
-              </a>
-            </p>
-          </div>
-        </main>
-        <Footer />
-      </div>
+        </div>
+        <div className="mt-8 bg-slate-200 py-8 text-center dark:bg-slate-800 sm:text-left">
+          <p className="mx-auto max-w-screen-md px-4 text-slate-800 dark:text-slate-400">
+            Contact me at{" "}
+            <a
+              href="mailto:camilorivera86@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="whitespace-nowrap text-lg font-semibold text-sky-600 dark:text-sky-500"
+            >
+              <MailIcon /> camilorivera86@gmail.com
+            </a>
+          </p>
+        </div>
+      </main>
+      <Footer />
     </>
   );
 }
