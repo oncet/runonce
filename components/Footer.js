90b575bd06635e301ba04cbc202649d9059f1489
oncet <camilorivera86@gmail.com>
2023-07-03 13:17:44 -0300

    update footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="break-inside-avoid px-4 py-8">
       <div className="mx-auto max-w-screen-md px-4">
-        <ul className="flex flex-col gap-8 text-right text-slate-600 dark:text-slate-400 sm:flex-row">
+        <ul className="flex flex-col justify-end gap-8 text-slate-600 dark:text-slate-400 sm:flex-row">
           <li className="hidden print:block">
             <Link
               href="/"
@@ -13,7 +13,7 @@ export default function Footer() {
               runonce.io
             </Link>
           </li>
-          <li>
+          <li className="hidden print:block">
             <a
               href="https://www.linkedin.com/in/camilorivera/"
               target="_blank"
@@ -23,7 +23,7 @@ export default function Footer() {
               linkedin.com/in/camilorivera
             </a>
           </li>
-          <li>
+          <li className="hidden print:block">
             <a
               href="https://github.com/oncet/"
               target="_blank"
@@ -33,7 +33,7 @@ export default function Footer() {
               github.com/oncet
             </a>
           </li>
-          <li>
+          <li className="hidden print:block">
             <a
               href="https://stackoverflow.com/users/1140065/camilo/"
               target="_blank"
@@ -41,6 +41,36 @@ export default function Footer() {
               className="transition duration-300 hover:text-slate-800 dark:hover:text-slate-300"
             >
               stackoverflow.com/users/1140065
+            </a>
+          </li>
+          <li className="block print:hidden">
+            <a
+              href="https://www.linkedin.com/in/camilorivera/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="transition duration-300 hover:text-slate-800 dark:hover:text-slate-300"
+            >
+              LinkedIn
+            </a>
+          </li>
+          <li className="block print:hidden">
+            <a
+              href="https://github.com/oncet/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="transition duration-300 hover:text-slate-800 dark:hover:text-slate-300"
+            >
+              GitHub
+            </a>
+          </li>
+          <li className="block print:hidden">
+            <a
+              href="https://stackoverflow.com/users/1140065/camilo/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="transition duration-300 hover:text-slate-800 dark:hover:text-slate-300"
+            >
+              Stack Overflow
             </a>
           </li>
         </ul>
