8f6d8aca21a577ee68534c4102c86ae5cbbe3678
oncet <camilorivera86@gmail.com>
2023-07-04 00:38:47 -0300

    wip

--- pages/photo/[id].js
+++ pages/photo/[id].js
@@ -40,13 +40,21 @@ export default function Home({ photo }) {
         />
         <meta property="og:image" content="https://runonce.io/og.jpg" />
       </Head>
-      <header className="mx-auto mb-4 max-w-screen-md px-6 pt-4 sm:pt-8">
-        <h1 className="text-4xl font-extrabold lowercase tracking-tight">
-          Photo #{router.query.id}
-        </h1>
-      </header>
-      <main>
-        <Image src={photo.src} alt={photo.alt} priority />
+      <div className="mx-auto max-w-screen-lg px-2">
+        <header className="mx-auto max-w-screen-md pb-4 pt-4">
+          <h1 className="text-xl font-thin lowercase tracking-wide text-slate-400">
+            Photo #{router.query.id}
+          </h1>
+        </header>
+      </div>
+      <main className="mx-auto max-w-screen-md">
+        <Image
+          src={photo.src}
+          alt={photo.alt}
+          priority
+          quality={100}
+          className="max-h-full max-w-full"
+        />
       </main>
     </>
   );
