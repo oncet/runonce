1cd466449eef05371ba513ceb5ff0b5ff00021f0
oncet <camilorivera86@gmail.com>
2023-07-07 00:26:12 -0300

    adjust z index of menu

--- components/ToggleDarkModeButton.js
+++ components/ToggleDarkModeButton.js
@@ -70,7 +70,7 @@ export default function ToggleDarkModeButton() {
           }
         >
           {isThemeMenuOpen && (
-            <ul className="absolute mt-2 flex flex-col gap-1 overflow-hidden rounded-full bg-slate-200 text-center  dark:bg-slate-800">
+            <ul className="absolute z-40 mt-2 flex flex-col gap-1 overflow-hidden rounded-full bg-slate-200 text-center  dark:bg-slate-800">
               {Object.keys(themeIcons).map((themeName) => (
                 <li key={themeName}>
                   <button
