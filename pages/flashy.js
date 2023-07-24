4ad2d7d9e25ba858f8fa5d51d6ef43906aefb298
oncet <camilorivera86@gmail.com>
2023-07-23 21:17:20 -0300

    update flashy page

--- pages/flashy.js
+++ pages/flashy.js
@@ -14,7 +14,7 @@ export default function Home() {
         <meta property="og:image" content="https://runonce.io/og.jpg" />
       </Head>
       <main className="mx-auto flex w-full max-w-screen-md grow flex-col justify-center px-4">
-        <h1 className="bg-gradient-to-r from-cyan-500 to-blue-500 bg-clip-text text-center text-9xl font-extrabold uppercase tracking-wider text-slate-900 underline decoration-white decoration-wavy decoration-8 underline-offset-[20px] [-webkit-text-stroke:4px_transparent] [text-decoration-skip-ink:none] [text-shadow:white_1px_0_10px]">
+        <h1 className="bg-gradient-to-r from-cyan-500 to-blue-500 bg-clip-text text-center text-9xl font-extrabold uppercase italic tracking-wider text-slate-900 decoration-white decoration-wavy decoration-8 underline-offset-[20px] [text-decoration-skip-ink:none] [text-shadow:white_1px_0_10px] [-webkit-text-stroke:4px_transparent]">
           Runonce
         </h1>
       </main>
