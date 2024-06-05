51c1eb2f7d52814b445892cb0fb7fee1ad8a1df5
oncet <camilorivera86@gmail.com>
2024-06-05 00:31:52 -0300

    update card

--- components/Card.js
+++ components/Card.js
@@ -14,28 +14,30 @@ export default function Card({ title, subtitle, summary, children }) {
   return (
     <div
       className={
-        "flex break-inside-avoid flex-col overflow-hidden rounded-lg border-2 bg-slate-50 pb-3 transition-all duration-300 [-webkit-tap-highlight-color:transparent] terminal:rounded-none terminal:border-[2px] terminal:border-dashed	terminal:border-orange-500 terminal:bg-black dark:border-slate-800/75 dark:bg-slate-800/75 print:border-none print:p-0 " +
+        "flex break-inside-avoid flex-col overflow-hidden rounded-lg border-2 bg-slate-50 transition-all duration-300 [-webkit-tap-highlight-color:transparent] terminal:rounded-none terminal:border-[2px] terminal:border-dashed	terminal:border-orange-500 terminal:bg-black dark:border-slate-800/75 dark:bg-slate-800/75 print:border-none print:p-0 " +
         (isOpen ? "border-slate-400" : "border-slate-200")
       }
     >
       <div className="flex flex-col gap-4">
         <div className="">
-          <button
+          <div
             className={
-              "group flex w-full cursor-pointer justify-between pb-3 pl-5 pr-3 pt-3 transition duration-300 focus-visible:rounded-t-lg dark:hover:bg-slate-800 " +
-              (isOpen ? "dark:bg-slate-800" : "")
+              "group flex w-full justify-between pb-3 pl-5 pr-3 pt-3 transition duration-300 focus-visible:rounded-t-lg  "
             }
-            onClick={onClickHandler}
-            aria-label="Expand card"
           >
             <h2 className="text-xl font-semibold">{title}</h2>
-            <div className="print:hidden">
-              <ToggleExpandIcon isOpen={isOpen} />
-            </div>
-          </button>
+          </div>
           <div className="px-5">{subtitle}</div>
         </div>
         <div className="px-3">{summary}</div>
+        <button
+          className="flex justify-center rounded-b-lg pb-4"
+          onClick={onClickHandler}
+          aria-label="Expand card"
+          title={`${isOpen ? "Collapse" : "Expand"} ${title} card`}
+        >
+          <ToggleExpandIcon isOpen={isOpen} />
+        </button>
       </div>
       <div
         className={
@@ -44,7 +46,7 @@ export default function Card({ title, subtitle, summary, children }) {
         }
       >
         <div className="row-[1_/_span_2] overflow-hidden">
-          <div className="mb-2 mt-5">{children}</div>
+          <div className="mb-5">{children}</div>
         </div>
       </div>
     </div>
--- components/icons/ToggleExpandIcon.js
+++ components/icons/ToggleExpandIcon.js
@@ -2,25 +2,20 @@ export default function ToggleExpandIcon({ isOpen }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
+      fill="none"
       viewBox="0 0 24 24"
-      fill="currentColor"
+      strokeWidth={1.5}
+      stroke="currentColor"
       className={
-        "h-6 w-6 fill-slate-600 transition duration-300 terminal:fill-orange-500 group-hover:dark:fill-slate-400"
+        "h-6 w-6 stroke-slate-400 transition duration-300 terminal:stroke-orange-500 " +
+        (isOpen ? "scale-y-[-1]" : "")
       }
     >
-      {isOpen ? (
-        <path
-          fillRule="evenodd"
-          d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm3 10.5a.75.75 0 0 0 0-1.5H9a.75.75 0 0 0 0 1.5h6Z"
-          clipRule="evenodd"
-        />
-      ) : (
-        <path
-          fillRule="evenodd"
-          d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z"
-          clipRule="evenodd"
-        />
-      )}
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+      />
     </svg>
   );
 }
