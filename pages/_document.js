9b43ed21742c00d564c03b68b59afbbdecd624f2
oncet <camilorivera86@gmail.com>
2024-06-09 12:22:12 -0300

    remove mono spaced font

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,10 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="flex flex-col bg-white text-slate-900 terminal:bg-black terminal:font-mono terminal:text-yellow-400 dark:bg-slate-900 dark:text-slate-200 print:!text-black">
+      <body className="flex flex-col bg-white text-slate-900 terminal:bg-black terminal:text-yellow-400 dark:bg-slate-900 dark:text-slate-200 print:!text-black">
         <Main />
         <NextScript />
       </body>
