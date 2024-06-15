cc730ce6daf557c12cf4db3c2fe26886dbf7c26f
oncet <camilorivera86@gmail.com>
2024-06-15 00:30:48 -0300

    switch links to list

--- pages/index.js
+++ pages/index.js
@@ -20,23 +20,24 @@ export default function Home() {
         <p className="mb-4 text-lg [text-wrap:balance] dark:text-slate-300">
           Hi, welcome to my homepage! 👋 My name is Camilo and I like coding.
         </p>
-        <p className="mb-20 text-lg [text-wrap:balance] dark:text-slate-300">
-          Checkout my{" "}
-          <Link
-            href="resume"
-            className="font-bold text-sky-600 dark:text-sky-400"
-          >
-            resume
-          </Link>{" "}
-          or my recent{" "}
-          <Link
-            href="projects"
-            className="font-bold text-sky-600 dark:text-sky-400"
-          >
-            projects
-          </Link>
-          .
-        </p>
+        <ul className="mb-20 flex gap-4 text-lg [text-wrap:balance] dark:text-slate-300">
+          <li>
+            <Link
+              href="projects"
+              className="inline-block rounded-lg border-2 px-3 py-1 font-bold text-sky-600 transition-colors dark:border-slate-700 dark:bg-slate-700 dark:text-sky-400 dark:hover:border-slate-500 dark:hover:text-slate-200"
+            >
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="resume"
+              className="inline-block rounded-lg border-2 px-3 py-1 font-bold text-sky-600 transition-colors dark:border-slate-700 dark:bg-slate-700 dark:text-sky-400 dark:hover:border-slate-500 dark:hover:text-slate-200"
+            >
+              Resume
+            </Link>
+          </li>
+        </ul>
       </main>
     </>
   );
